c58c29321a10f5b8c6b89c7e4ca74fc790239888
Saad Guessous <guessous_saad@hotmail.com>
2017-11-20 21:33:01 +0200

    added projects page

--- server.js
+++ server.js
@@ -45,6 +45,13 @@ app.get('/about', (req, res) => {
     })
 })
 
+app.get('/projects', (req, res) => {
+    res.render('projects.hbs', {
+        pageTitle: 'Projects Page',
+        message: 'Portfolio page here'
+    })
+})
+
 app.get('/bad', (req, res) => {
     res.send({
         errorMessage: 'Unable to handle request'
